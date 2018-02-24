034465f9677e1e3df0c1952653634faa64bc9f06
Emil Rosenius Pedersen <emil0srp@gmail.com>
2018-02-24 16:48:52 +0100

    SETUP: Added AMD and UFE support

--- vue-units.js
+++ vue-units.js
@@ -1,8 +1,31 @@
-module.exports = {
-    install: function (Vue, options) {
+(function () {
 
+    /**
+     * Install plugin
+     * @param Vue
+     * @param options
+     */
+
+    function plugin(Vue, options) {
+
+        /*
+         * Check if plugin is already installed
+         */
+        if (plugin.installed) {
+            console.warn('It seems like you\'re trying to install vue-units twice.')
+            return;
+        }
+        plugin.installed = true;
+
+        /*
+         * Override the convert-units plugin, if another is defined
+         */
         var units = options && options.units ? options.units : require('convert-units');
 
+        /*
+         * Make the instance of convert-units available on the vue instance
+         * using this.$units.
+         */
         Object.defineProperties(Vue.prototype, {
             $units: {
                 get: function () {
@@ -11,11 +34,33 @@ module.exports = {
             }
         });
 
+        /*
+         * Make the instance of convert units available on the global
+         * Vue instance.
+         */
         Vue.units = units;
 
+        /*
+         * Add a simple filter for converting units
+         */
         Vue.filter('units', function (value, from, to) {
-            return units(value).from(from).to(to);
+            try {
+                return units(value).from(from).to(to);
+            } catch (err) {
+                // Prevent Vue from crashing if incorrect metrics are provided
+                // and simply return the original value instead.
+                console.error(err);
+                return value;
+            }
         });
-
     }
-};
+
+    if (typeof exports == "object") {
+        module.exports = plugin
+    } else if (typeof define == "function" && define.amd) {
+        define([], function(){ return plugin })
+    } else if (window.Vue) {
+        Vue.use(plugin)
+    }
+
+})();
